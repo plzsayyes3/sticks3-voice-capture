227c9e63118eaa4646121f4eac4e17623cfbd3ca
plzsayyes3 <102906997+plzsayyes3@users.noreply.github.com>
2026-09-15 21:57:18 +0900

    feat: expose recording failure callback

--- firmware/components/audio_pipeline/include/audio_pipeline.h
+++ firmware/components/audio_pipeline/include/audio_pipeline.h
@@ -3,7 +3,11 @@
 #include <stdint.h>
 #include "esp_err.h"
 
+typedef void (*audio_pipeline_error_callback_t)(esp_err_t error);
+
 esp_err_t audio_pipeline_init(void);
+void audio_pipeline_set_error_callback(audio_pipeline_error_callback_t callback);
 esp_err_t audio_pipeline_start(uint32_t session_id);
 esp_err_t audio_pipeline_stop(void);
+esp_err_t audio_pipeline_last_error(void);
 uint32_t audio_pipeline_session_id(void);
